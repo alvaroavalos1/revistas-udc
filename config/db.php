338fa002916769106b4c55d4a8ea257af548d4e1
alvaroavalos1 <alvaroemm06@gmail.com>
2026-06-01 17:05:26 -0600

    configuracion despliegue automatico FTP

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host     = 'localhost';
-$dbname   = 'plataforma_revistas';
-$user     = 'root';
-$password = '';
+$host     = 'sql305.infinityfree.com';
+$dbname   = 'if0_42050746_revistas';
+$user     = 'if0_42050746';
+$password = 'S6Q1bpOaiT';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
